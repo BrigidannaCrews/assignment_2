0c6c205f0a87558dd06662c2ecdf9cfc0c8ba58f
xiuweili1999 <60372954+xiuweili1999@users.noreply.github.com>
2020-03-31 18:35:34 -0400

    Moved pics to images folder

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
 
-for (var i = 1; i <= 5; i++){
-    const newImage = document.createElement('img');
-    const path = 'pic' + i + '.jpg' ;
-    newImage.setAttribute('src', path);
-    thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', displayImage)
+for (var i = 1; i <= 5; i++) {
+  const newImage = document.createElement("img");
+  const path = "images/pic" + i + ".jpg";
+  newImage.setAttribute("src", path);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener("click", displayImage);
 }
 
 function displayImage(clickedImage) {
-    const imagePath = clickedImage.target.getAttribute('src');
-    displayedImage.setAttribute('src', imagePath);
-};
+  const imagePath = clickedImage.target.getAttribute("src");
+  displayedImage.setAttribute("src", imagePath);
+}
 
 /* Wiring up the Darken/Lighten button */
